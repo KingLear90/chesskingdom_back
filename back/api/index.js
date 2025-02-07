f40ff596e147403bae13f9fb8485d73d55bc42c0
Alan S. <lic.sanchezalan@gmail.com>
2025-02-07 17:09:13 -0300

    delete extra comment

--- back/api/index.js
+++ back/api/index.js
@@ -54,23 +54,6 @@ app.use(
     })
 )
 
-/* const allowedOrigins = [
-    'https://chesskingdom.vercel.app',
-    'http://localhost:5173',
-    'http://localhost:3001'
-];
-
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true
-})); */
-
 app.options('*', cors()); 
 
 // Permitir solicitudes desde tu frontend en Vercel
