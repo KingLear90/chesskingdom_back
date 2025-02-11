2a0baa1396ed81cbc22b3d66f79f8a07b5d847b5
Alan S. <lic.sanchezalan@gmail.com>
2025-02-11 09:47:12 -0300

    try catch new approach

--- back/src/controllers/problemController.js
+++ back/src/controllers/problemController.js
@@ -2,11 +2,13 @@ import problem from "../models/problemModel.js"
 
 export const getRandomProblem = async (req, res) => {
     try {
-        const randomProblem = await problem.aggregate([{ $sample: { size: 1 } }]);
-        console.log('Random problem: ', randomProblem);
-        res.status(200).json(randomProblem);
+        const randomProblem = await problem.find({}).limit(1);
+        if (randomProblem.length > 0) {
+            console.log('Random problem: ', randomProblem);
+            return res.status(200).json(randomProblem);
+        }
     } catch (error) {
-        console.error(error);
+        console.error("Error fetching problem" , error);
         res.status(500).json({ message: "Internal server error", error });
     }
 }
